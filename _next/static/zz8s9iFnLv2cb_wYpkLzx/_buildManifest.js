22e978385734bf51ec96d9ce02a0a8f4db1530e3
Arnas Savickas <arnas.savi@gmail.com>
2021-04-12 19:51:16 +0300

    Updates

---
+++ _next/static/zz8s9iFnLv2cb_wYpkLzx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/290-8a9b294872ae526dc103.js","static/css/156b68fad41fb4d77e53.css","static/chunks/pages/index-f9d5795973fbd861eeab.js"],"/_error":["static/chunks/pages/_error-bd2bb2c5a8f1670977b4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
